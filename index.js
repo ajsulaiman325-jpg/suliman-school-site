41f0b78f88c04910238674ab2e84044cb5a0dd2e
ajsulaiman325-jpg <ajsulaiman325@gmail.com>
2026-08-15 09:22:44 +0300

    my school site

---
+++ index.js
@@ -0,0 +1,7 @@
+
+        function openEmailModal() {
+            document.getElementById('emailModal').style.display = 'flex';
+        }
+        function closeEmailModal() {
+            document.getElementById('emailModal').style.display = 'none';
+        }
